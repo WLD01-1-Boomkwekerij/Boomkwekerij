b7f7d17d0ef531b999ff37a2b13099c99a32bc0b
michael <michael99access@outlook.com>
2016-11-16 15:16:11 +0100

    Initial Commit

---
+++ Pagina's/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+        <link a href="../Css/style.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+    <center><div id="wrapper">
+            <div id="header">
+                <h2>Header</h2>
+
+            </div>
+
+            <div id="menu">
+                <nav>
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Klimplanten</a></li>
+                        <li><a href="#">Cytisus</a></li>
+                        <li><a href="#">Ilex</a></li>
+                        <li><a href="#">Heesters</a></li>
+                        <li><a href="#">Contact</a></li>
+
+
+
+                    </ul>
+                </nav>
+
+
+
+            </div>
+            <div id="main">
+                <h2>Hoofdkader</h2>
+                
+            </div>
+            <div id="side"><h2>Sidebalk</h2>
+            
+            
+            </div>
+            
+            <div id="footer"></div>
+
+        </div>
+    </center>
+</body>
+</html>
