0849a765e7c4eb2bb7a1e61349a54727a5d22f34
Erwin Jacobs <erwinpj@outlook.com>
2016-11-17 16:11:42 +0100

    Aangepaste content

--- Pages/Home.php
+++ Pages/Home.php
@@ -21,12 +21,25 @@
                 </section>
             </section>
             <section id="mid">
-                <section id="rightmenu">
-                    B uifrh guierhg rseiugohuih gu
-                </section>
                 <section id="maincontent">
                     Bku iej fioejfoeifj dsiofjdsif jdsoi j
                 </section>
+                <section id="rightmenu">
+                   <h3>Contact informatie</h3>
+                   <ul id="contact informatie">
+                       <li>Fa. P. Boer</li>
+                       <li>Rijneveld 125<br>2771 XV Boskoop</li>
+                       <li>B.G.G:<br>0031 (0)172217308</li>
+                       <li>Peter Boer:<br>0031 (0)657915055</li>
+                       <li>Robert Boer:<br>0031(0)622442190</li>
+                       <li>fax:<br>0031 (0)172216827</li>
+                       <li>E-mail:<br>info@boomkwekerijpboer.nl</li>
+                   </ul>
+                   <h3>Groen-Direkt Boskoop</h3>
+                   Geen opkomende evenementen<br>
+                   <a href="http://www.groen-direkt.nl/home-nl">link</a>
+                </section>
+
             </section>
         </section>
 
