33ca34bd8be894f53039a2f5f15add5dcc13fe88
michael <michael99access@outlook.com>
2016-11-17 14:15:10 +0100

    Initial commit , deze keer wel goed

---
+++ Pages/Home.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Boomkwekerij</title>
+        <link href="../Css/MainStyle.css" rel="stylesheet" type=text/css"">
+        <link href="../Css/HomeStyle.css" rel="stylesheet" type="text/css">
+    </head>
+    <body>
+        <section id="wrapper">
+            <section id="top">
+                <section id="header"></section>
+                <section id="topmenu">
+                    <ul>
+                        <li><a href="#">Home</a></li>
+                        <li><a href="#">Nieuws</a></li>
+                        <li><a href="#">Catalogus</a></li>
+                        <li><a href="#">Prijslijst</a></li> 
+                        <li><a href="#">Contact</a></li>
+                    </ul>
+                </section>
+            </section>
+            <section id="mid">
+                <section id="rightmenu">
+                    B uifrh guierhg rseiugohuih gu
+                </section>
+                <section id="maincontent">
+                    Bku iej fioejfoeifj dsiofjdsif jdsoi j
+                </section>
+            </section>
+        </section>
+
+        <section id="footer">
+
+        </section>
+    </body>
+</html>
