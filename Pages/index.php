f0729d56cff3d4e1639e1a14ebfd4165ef89b63b
michael <michael99access@outlook.com>
2016-11-28 12:40:03 +0100

    Vertalingen

--- Pages/index.php
+++ Pages/index.php
@@ -6,6 +6,10 @@
         <title>Boomkwekerij - Home</title>
         <link href="../Css/MainStyle.css" rel="stylesheet" type="text/css">
         <link href="../Css/HomeStyle.css" rel="stylesheet" type="text/css">
+   
+      
+    
+    
     </head>
     <body>
         <section id="wrapper">
@@ -24,6 +28,11 @@
             </section>
             <section id="mid">
                 <section id="rightmenu">
+                     <div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'nl', includedLanguages: 'en,it,nl,sv', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
                     <h3>Contact informatie</h3>
                     <ul id="contact_information">
                         <li>Fa. P. Boer</li>
@@ -33,24 +42,24 @@
                         <li>Robert Boer:<br>0031(0)622442190</li>
                         <li>fax:<br>0031 (0)172216827</li>
                         <li>E-mail:<br>info@boomkwekerijpboer.nl</li>
+                        
                     </ul>
                     <h3>Groen-Direkt Boskoop</h3>
                     Geen opkomende evenementen<br>
                     <a href="http://www.groen-direkt.nl/home-nl" TARGET="_blank">link</a>
                 </section>
                 <section id="maincontent">
+                   
+        
                     <h1>Welkom op de site</h1>
-                    Bku iej fioejfoeifj dsiofjdsif jdsoi j
+                   Nazi
 
-                    <h2>Lorem ipsum</h2>
-
-                    <p>L Lorem ipsum dolor sit <br>amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui.</p>
-
-                    <p>Etiam rhoncus. Maecenas tempus,<br> tellus eget condimentum rhoncus, sem quam semper <br>libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend sapien. Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui. Cras ultricies mi eu turpis hendrerit fringilla. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; In ac dui quis mi consectetuer lacinia.</p>
-
-                    <p>Nam pretium turpis et arcu. Duis arcu <br> tortor, suscipit eget, imperdiet nec, imperdiet iaculis, ipsum. Sed aliquam ultrices mauris. Integer ante arcu, accumsan a, consectetuer eget, posuere ut, mauris. Praesent adipiscing. Phasellus ullamcorper ipsum rutrum nunc. Nunc nonummy metus. Vestibulum volutpat pretium libero. Cras id dui. Aenean ut eros et nisl sagittis vestibulum. Nullam nulla eros, ultricies sit amet, nonummy id, imperdiet feugiat, pede. Sed lectus. Donec mollis hendrerit risus. Phasellus nec sem in justo pellentesque facilisis. Etiam imperdiet imperdiet orci. Nunc nec neque. Phasellus leo dolor, tempus non, auctor et, hendrerit quis, nisi.</p>
-
-                    <p>Curabitur ligula sapien, tincidunt non, </p>> 
+<p>Embleem van de nationaalsocialisten en latere Rijksvlag van nazi-Duitsland, 1935-1945
+Een nazi is een aanhanger van het nationaalsocialisme. Het nationaalsocialisme is de ideologie waarop, tussen 1933 en 1945, in Duitsland een totalitaire dictatuur was gebaseerd. De grondlegger van het nationaalsocialisme was de oorspronkelijk Duits/Oostenrijkse politieke provocateur, populist en latere dictator van Duitsland Adolf Hitler, die ook aan het hoofd stond van de politieke partij waarin het nationaalsocialisme zich georganiseerd had: de Nationaalsocialistische Duitse Arbeiderspartij (NSDAP).
+Onderscheid[bewerken]
+Vaak worden de dictaturen van Spanje en Italië uit dezelfde tijdsperiode, die weliswaar een samenwerkingsverband met de nazi's hadden, onterecht over één kam geschoren met het nationaalsocialisme en de nazi's. De overeenkomst was dat deze landen ook dictaturen waren maar ze hadden niet de extreem racistische elementen van het nazisme. De Spaanse generaal Francisco Franco steunde op een sterk nationalistische ideologie voor zijn dictatoriale bewind over Spanje, het zogenaamde Franquisme (een verbond tussen de Spaanse katholieke kerk, het leger, de conservatieve Spaanse elite en geleid door generalissimo dictator Franco) zonder dat daar een sterk nazi-racistisch element in aanwezig was. Evenmin was de Italiaanse Benito Mussolini een nazi, maar een fascist dat evenals het Franquisme sterk nationalistisch gericht was. Het Italiaanse fascisme steunde evenals het Franquisme sterk op het nationalisme maar had ook socialistische trekjes zoals corporatistische en collectivistische elementen en stond, zoals bij de nazi's, voor een sterk land met een sterke leider. Democratie werd ook hier verworpen maar afkomst van de burgers speelde niet zoveel mee, als men maar achter de Italiaanse staat en de fascisten stond. Hitler, die in zijn beginjaren een bewonderaar van Mussolini was, heeft veel elementen van het fascisme, zoals het corporatisme, in zijn nazistische ideologie geïntegreerd. Later in de oorlog onder invloed van Hitler werden er wel racistische wetten geïntroduceerd. Mussolini werd aan het eind van de oorlog door zijn eigen fascistische partij afgezet. Na de oorlog werd het Duitse nazisme en Italiaanse fascisme verboden. In Spanje echter, dat tijdens de Tweede Wereldoorlog neutraal bleef, hield het dictatoriale bewind van Francisco Franco tot in de jaren zeventig stand.
+Woordverklaring[bewerken]
+Het woord 'nazi' is afkomstig van Nationalsozialist, de Duitstalige term voor een aanhanger van het nationaalsocialisme. De afkorting 'nazi' is analoog aan het iets oudere 'sozi', wat in het Duitsland van het begin van de 20e eeuw de volksnaam was voor een aanhanger van het socialisme. De term 'nazisme' is van 'nazi' afgeleid en wordt tegenwoordig gebruikt als afkorting voor nationaalsocialisme. Men gebruikt het ook als pejoratief bijvoeglijk naamwoord ('dat zijn nazistische praktijken').</p>
                 </section>
             </section>
         </section>
--- Pages/login.php
+++ Pages/login.php
@@ -30,6 +30,11 @@
                 </section>
                 <section id="mid">
                     <section id="rightmenu">
+                         <div id="google_translate_element"></div><script type="text/javascript">
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'nl', includedLanguages: 'en,it,nl,sv', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
+}
+</script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
                         <h3>Catalogus</h3>
                         <ul id="catalogus">
                             <li></li>
