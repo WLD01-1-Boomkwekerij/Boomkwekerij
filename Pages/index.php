b7ae8846dee738cac1febc98f0cbd9e6b00fb998
wesley pleijsier <pleijsierwesley@live.nl>
2016-11-29 10:08:41 +0100

    css changes voor google translate

--- Pages/index.php
+++ Pages/index.php
@@ -2,6 +2,7 @@
 <html>
     <head>
         <meta charset="UTF-8">
+        <meta name="google" value="notranslate">
         <title>Boomkwekerij - Home</title>
         <!-- Icon Pack -->
         <link rel="stylesheet" href="../font-awesome-4.7.0/css/font-awesome.min.css">
