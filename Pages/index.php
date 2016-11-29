ad8d7a480194e95c69c5df5d01c356be73ea8a93
Erwin Jacobs <erwinpj@outlook.com>
2016-11-29 13:56:33 +0100

    Achternaamen worden nu niet meer meevertaald

--- Pages/index.php
+++ Pages/index.php
@@ -2,10 +2,10 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Boomkwekerij - Home</title>
+        <title class="notranslate">Boomkwekerij - Home</title>
         <!-- Icon Pack -->
         <link rel="stylesheet" href="../font-awesome-4.7.0/css/font-awesome.min.css">
-        
+
         <link href="../Css/MainStyle.css" rel="stylesheet" type="text/css">
         <link href="../Css/HomeStyle.css" rel="stylesheet" type="text/css"> 
         <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
@@ -35,10 +35,10 @@
                         <li><a href="../pages/pricelist.php">Prijslijst</a></li> 
                         <li><a href="../pages/contact.php">Contact</a></li>
                         <?php
-                        if(isset($_SESSION['logged_in'])){
+                        if (isset($_SESSION['logged_in'])) {
                             print("<li><a href='../pages/logged_in.php'>Beheerderspagina</a></li>");
                         }
-                         ?>
+                        ?>
                     </ul>
                 </section>
             </section>
@@ -51,16 +51,15 @@
                         }
                     </script>
                     <h3>Contact informatie</h3>
-                    <ul id="contact_information">
-                        <li>Fa. P. Boer</li>
-                        <li>Rijneveld 125<br>2771 XV Boskoop</li>
-                        <li>B.G.G:<br>0031 (0)172217308</li>
-                        <li>Peter Boer:<br>0031 (0)657915055</li>
-                        <li>Robert Boer:<br>0031(0)622442190</li>
-                        <li>fax:<br>0031 (0)172216827</li>
-                        <li>E-mail:<br>info@boomkwekerijpboer.nl</li>
-
-                    </ul>
+                        <ul class="notranslate" id="contact_information">
+                            <li>Fa. P. Boer</li>
+                            <li>Rijneveld 125<br>2771 XV Boskoop</li>
+                            <li>B.G.G:<br>0031 (0)172217308</li>
+                            <li>Peter Boer:<br>0031 (0)657915055</li>
+                            <li>Robert Boer:<br>0031(0)622442190</li>
+                            <li>fax:<br>0031 (0)172216827</li>
+                            <li>E-mail:<br>info@boomkwekerijpboer.nl</li>
+                        </ul>
                     <h3>Groen-Direkt Boskoop</h3>
                     Geen opkomende evenementen<br>
                     <a href="http://www.groen-direkt.nl/home-nl" TARGET="_blank">link</a>
@@ -68,34 +67,34 @@
                 <section id="maincontent">
 
                     <!-- Hierin moet de tekst van de site, het id moet het TextID bevatten om text op te slaan -->
-                    
+
                     <div>
                         <button class="fa fa-bold" onclick="markupText('bold')"></button>
-            <button class="fa fa-italic" onclick="markupText('italic')"></button>
-            <button class="fa fa-underline" onclick="markupText('underline')"></button>
-            <button class="fa fa-align-left" onclick="markupText('justifyLeft')"></button>
-            <button class="fa fa-align-justify" onclick="markupText('justifyCenter')"></button>
-            <button class="fa fa-align-right" onclick="markupText('justifyRight')"></button>
-            <button class="fa fa-list-ol" onclick="markupText('insertOrderedList')"></button>
-            <button class="fa fa-list-ul" onclick="markupText('insertUnorderedList')"></button>
-            <!--<select class="numberPicker" onchange="setFontSize('textID1', 5)"></select>-->
-            <!--<button class="fa fa-link" onclick="createLink(this)"></button>-->
+                        <button class="fa fa-italic" onclick="markupText('italic')"></button>
+                        <button class="fa fa-underline" onclick="markupText('underline')"></button>
+                        <button class="fa fa-align-left" onclick="markupText('justifyLeft')"></button>
+                        <button class="fa fa-align-justify" onclick="markupText('justifyCenter')"></button>
+                        <button class="fa fa-align-right" onclick="markupText('justifyRight')"></button>
+                        <button class="fa fa-list-ol" onclick="markupText('insertOrderedList')"></button>
+                        <button class="fa fa-list-ul" onclick="markupText('insertUnorderedList')"></button>
+                        <!--<select class="numberPicker" onchange="setFontSize('textID1', 5)"></select>-->
+                        <!--<button class="fa fa-link" onclick="createLink(this)"></button>-->
                     </div>
-                    
+
                     <div class="ContentEditable" id="textID1">
                         <?php
-                                //Gebruik dit commando met de TextID van de tekst om hem altijd te laten werken
-                                print(loadTextFromDB(1));
+                        //Gebruik dit commando met de TextID van de tekst om hem altijd te laten werken
+                        print(loadTextFromDB(1));
                         ?>
                     </div>
                 </section>
             </section>
         </section>
-        <section id="footer">
+        <section  class="notranslate" id="footer">
             <?php
-            if(isset($_SESSION['logged_in'])){
+            if (isset($_SESSION['logged_in'])) {
                 print("<li><a href='../Php/loggout.php'>Uitloggen</a></li>");
-            }else{
+            } else {
                 print("<li><a href='../pages/login.php'>Inloggen</a></li>");
             }
             ?>
