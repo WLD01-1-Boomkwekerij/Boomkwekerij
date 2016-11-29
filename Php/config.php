00815263ec37b80e4cafe8c37885018551b92a68
michael <michael99access@outlook.com>
2016-11-29 13:28:14 +0100

    no message

--- Php/config.php
+++ Php/config.php
@@ -1,41 +1,46 @@
 <?php
 
-$db = "boomkwekerij";
-$user = "root";
-$pass = "usbw";
-$pdo = new PDO($db, $user, $pass);
-echo"nee";
-if(isset($_POST['nr'])&& isset($_POST['potmaat'])&& isset($_POST['hoogte'])&& isset($_POST['prijskwek'])&& isset($_POST['prijsvba'])&& isset($_POST['percc'])&& isset($_POST['perlaag'])
-        && isset($_POST['pertray'])){
-
-$nr = $_POST['nr'];    
-$potmaat = $_POST['potmaat'];
-$hoogte = $_POST['hoogte'];
-$prijskwek = $_POST['prijskwek'];
-$prijsvba = $_POST['prijsvba'];
-$percc = $_POST['percc'];
-$perlaag = $_POST['perlaag'];
-$pertray = $_POST['pertray'];
-
-
-
+$servername = "localhost";
+$username = "root";
+$password = "usbw";
+$dbname = "boomkwekerij";
 
 try {
-    $conn = new PDO("mysql:host=localhost;dbname=boomkwekerij", 'root', 'usbw');
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO boomkwekerij (CategoryID, Potmaat, PrijsKwekerij, PrijsVBA, ProductenCC, ProductenLaag, ProductenTray)" . "VALUES ('$nr', '$potmaat','$hoogte', '$prijskwek','$prijsvba','$percc','$perlaag','$pertray')";
-    // use exec() because no results are returned
-    $conn->exec($sql);
-    echo "New record created successfully";
-    }
-catch(PDOException $e)
-    {
-    echo  $e->getMessage();
-    }
 
-$conn = null;
+    // prepare sql and bind parameters
+    $stmt = $conn->prepare("INSERT INTO Prijs (CategoryID, Potmaat, Hoogte_max, PrijsKwekerij,
+        PrijsVBA, ProductenCC, ProductenLaag, ProductenTray) 
+    VALUES (:CategoryID, :Potmaat, :Hoogte, :PrijsKwekerij, :PrijsVBA, :ProductenCC, :ProductenLaag,
+    :ProductenTray)");
+    $stmt->bindParam(':CategoryID', $CategoryID);
+    $stmt->bindParam(':Potmaat', $Potmaat);
+    $stmt->bindParam(':Hoogte', $Hoogte);
+    $stmt->bindParam(':PrijsKwekerij', $prijsKwekerij);
+    $stmt->bindParam(':PrijsVBA', $prijsVBA);
+    $stmt->bindParam(':ProductenCC', $ProductenCC);
+    $stmt->bindParam(':ProductenLaag', $ProductenLaag);
+    $stmt->bindParam(':ProductenTray', $ProductenTray);
 
+    // insert a row
+    $CategoryID = $_POST['nr'];
+    $Potmaat = $_POST['potmaat'];
+    $Hoogte = $_POST['hoogte'];
+    $prijsKwekerij = $_POST['prijskwek'];
+    $prijsVBA = $_POST['prijsvba'];
+    $ProductenCC = $_POST['percc'];
+    $ProductenLaag = $_POST['perlaag'];
+    $ProductenTray = $_POST['pertray'];
+    $stmt->execute();
+
+
+
+
+    echo "New records created successfully";
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
 }
-
+$conn = null;
 ?>
