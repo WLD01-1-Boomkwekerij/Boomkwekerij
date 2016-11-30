7ec2a060e79555b8daa92ed467e050234bc5ca43
foepje12 <jellevanommen@hotmail.nl>
2016-11-30 23:18:11 +0100

    Nog een naamverandering

--- Php/DatabaseInformation.php
+++ Php/DatabaseInformation.php
@@ -5,7 +5,7 @@ include 'Database.php';
 function loadTextFromDB($textID) {
 
     $connection = connectToDatabase();
-    $statement = $connection->prepare("SELECT * FROM tekst WHERE ID=" . $textID);
+    $statement = $connection->prepare("SELECT * FROM tekst WHERE TEKSTID=" . $textID);
     $statement->execute();
 
     while ($row = $statement->fetch()) {
