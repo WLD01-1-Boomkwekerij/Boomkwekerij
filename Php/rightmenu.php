4203c4385416f137704ac517dae8860812c8d5e9
Erwin Jacobs <erwinpj@outlook.com>
2016-12-01 13:43:39 +0100

    de meest random kleine wijzigingen

--- Php/rightmenu.php
+++ Php/rightmenu.php
@@ -13,7 +13,3 @@ if (isset($_SESSION['logged_in'])) {
 }
 print("id = 'textID2'>".loadTextFromDB(2)."</div>");
 ?>
-<h3>Groen-Direkt Boskoop</h3>
-Geen opkomende evenementen<br>
-<a href="http://www.groen-direkt.nl/home-nl" TARGET="_blank">link</a>
-
