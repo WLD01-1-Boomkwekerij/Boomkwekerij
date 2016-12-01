c798692b8fdc0cee102f581fc487d1249a1028b3
Vescatur <ivan@edmi.nl>
2016-12-01 10:43:18 +0100

    catalogus yhea

--- Pages/catalog.php
+++ Pages/catalog.php
@@ -46,18 +46,31 @@
                     </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
                     <h3>Catalogus</h3>
                     <ul id="catalogus">
-                        <li>Plant1</li>
-                        <li>Plant2</li>
-                        <li>plant3</li>
-                        <li>plant4</li>
+                        <?php 
+                         $sqlCategory = getSQLArray("SELECT * FROM category");
+                        while($row = $sqlCategory->fetch()){
+                            $categoryNaam = $row["CategoryNaam"];
+                            $id = $row["CategoryID"];
+                            echo "<a href='http://localhost:8080/pages/catalog.php?category=$id'><li>$categoryNaam</li></a>";
+                        }
+                        ?>
                     </ul>
                 </section>
                 <section id="maincontent">
                     <?php
-                    $sql = getSQLArray("SELECT * FROM prijs");
-
-
-                    while ($row = $sql->fetch()) {
+                    if(!isset($_GET['category'])){
+                        $category = 1;
+                    }else {
+                        $category = $_GET['category'];
+                    }
+                    $sqlCategory = getSQLArray("SELECT * FROM category WHERE CategoryID = $category");
+                    $categoryRegel = $sqlCategory->fetch();
+                    $categoryNaam = $categoryRegel["CategoryNaam"];
+                    echo "<h1>$categoryNaam</h1>";  
+                    
+                    $sqlPrijs = getSQLArray("SELECT * FROM prijs WHERE CategoryID = $category");
+                    while ($row = $sqlPrijs->fetch()) {
+                        $prijsID = $row["PrijsID"];
                         $potmaat = $row["Potmaat"];
                         $hoogte = $row["Potmaat"];
                         $prijsKwekerij = $row["PrijsKwekerij"];
@@ -65,41 +78,49 @@
                         $perCC = $row["ProductenCC"];
                         $perLaag = $row["ProductenLaag"];
                         $perTray = $row["ProductenTray"];
-                        echo "<div class='item'>
-                        <div>
+                        $sqlPlant = getSQLArray("SELECT * FROM plant WHERE PrijsID = $prijsID");
+                        while ($plant = $sqlPlant->fetch()) {
+                            $naam = $plant['Naam'];
+                            echo "<div class='item'>
+                            <div>
                             <img src='/Catalogus fotos/Heesters/aucuba tray p13.jpg'>  
-                        </div>
-                        <table>
-                            <tr>
-                                <td>potmaat:</td>
-                                <td>$potmaat</td>
-                            </tr>
-                            <tr>
-                                <td>hoogte:</td>
-                                <td>$hoogte</td>
-                            </tr>
-                            <tr>
-                                <td>prijs kwekerij:</td>
-                                <td>$prijsKwekerij</td>
-                            </tr>
-                            <tr>
-                                <td>prijs VBA:</td>
-                                <td>$prijsVBA</td>
-                            </tr>
-                            <tr>
-                                <td>per cc:</td>
-                                <td>$perCC</td>
-                            </tr>
-                            <tr>
-                                <td>per laag:</td>
-                                <td>$perLaag</td>
-                            </tr>
-                            <tr>
-                                <td>per tray:</td>
-                                <td>$perTray</td>
-                            </tr>
-                        </table>
-                    </div>";
+                            </div>
+                                <table>
+                                    <tr>
+                                        <td>naam:</td>
+                                        <td>$naam</td>
+                                    </tr>
+                                    <tr>
+                                        <td>potmaat:</td>
+                                        <td>$potmaat</td>
+                                    </tr>
+                                    <tr>
+                                        <td>hoogte:</td>
+                                        <td>$hoogte</td>
+                                    </tr>
+                                    <tr>
+                                        <td>prijs kwekerij:</td>
+                                        <td>$prijsKwekerij</td>
+                                    </tr>
+                                    <tr>
+                                        <td>prijs VBA:</td>
+                                        <td>$prijsVBA</td>
+                                    </tr>
+                                    <tr>
+                                        <td>per cc:</td>
+                                        <td>$perCC</td>
+                                    </tr>
+                                    <tr>
+                                        <td>per laag:</td>
+                                        <td>$perLaag</td>
+                                    </tr>
+                                    <tr>
+                                        <td>per tray:</td>
+                                        <td>$perTray</td>
+                                    </tr>
+                                </table>
+                            </div>";
+                        }
                     }
                     ?>   
                 </section>
