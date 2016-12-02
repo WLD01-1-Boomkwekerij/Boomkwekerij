ee7729df9abe5519bb25f89e12e1de97d0c41e9d
foepje12 <jellevanommen@hotmail.nl>
2016-12-02 20:03:30 +0100

    Schermontwerp bestandsbeheerder, css moet nog verder aangepast worden

--- Pages/index.php
+++ Pages/index.php
@@ -9,9 +9,9 @@
         <?php
         session_start();
         if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
-        print("<link href='../Css/EditableStyle.css' rel='stylesheet' type='text/css'>");
-        print("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>");
-        print("<script src='../Javascript/InformationEditing.js'></script>");
+            print("<link href='../Css/EditableStyle.css' rel='stylesheet' type='text/css'>");
+            print("<script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>");
+            print("<script src='../Javascript/InformationEditing.js'></script>");
         }
         ?>
     </head>
@@ -42,30 +42,86 @@
                     print("</div>");
                     ?>
                     <br>
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    <!--BESTANDS BEHEERDER -->
-                    <div>
-                        
-                    </div>
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
                 </section>
                 
+                
+                
+                
+                <!--BESTANDS BEHEERDER -->
+                    <div id="FileManager">
+                        <div id="Files">
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>
+                        <div id="BottomInfo">
+                            <button style="
+                                    position: absolute;
+                                    margin-left: 5px;
+                                    border: none;
+                                    bottom: 5px;
+                                    "
+                                    >Cancel</button>
+                            <button style="
+                                    position: absolute;
+                                    margin-right: 5px;
+                                    border: none;
+                                    bottom: 5px;
+                                    right: 5px;
+                                    " 
+                                    >Select</button>
+                            <div style="
+                                    position: absolute;
+                                    left: 50%;
+                                    top: 10px;
+                                 ">
+                                <div id="currentPathSelected" style="
+                                     position: relative;
+                                     background-color: beige;
+                                     width: 350px;
+                                     height: 30px;
+                                     box-shadow: 0px 0px 2px 0px black;
+                                     left: -50%;
+                                     "
+                                     ></div>
+                            </div>
+
+                        </div>
+                    </div>
+                
+                
+                
+                
+
             </section>
         </section>
         <?php
