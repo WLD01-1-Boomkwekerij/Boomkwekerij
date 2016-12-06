9afdfc6beffd17fcbd0e6b8a5a364b3c6ffef778
michael <michael99access@outlook.com>
2016-12-06 13:44:47 +0100

    Opgelost Wesley hansje kan verwijderd worden

--- Pages/catalog.php
+++ Pages/catalog.php
@@ -114,6 +114,7 @@
                             <img src='/Catalogus fotos/Heesters/aucuba tray p13.jpg'>  
                             </div>
                                <input type='submit' name='btnvinkje' id='btnvinkje' value='&#x2714'>
+                               </form>
                             </div>";
                     }
                         
