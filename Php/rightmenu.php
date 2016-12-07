ce6243776f1882b5e740a1f1ab30ca26c1549daf
Erwin Jacobs <erwinpj@outlook.com>
2016-12-07 12:13:00 +0100

    translate issue gefixt

--- Php/rightmenu.php
+++ Php/rightmenu.php
@@ -1,9 +1,11 @@
+
 <div id="google_translate_element"></div><script type="text/javascript">
     function googleTranslateElementInit() {
         new google.translate.TranslateElement({pageLanguage: 'nl', includedLanguages: 'en,it,nl,sv', layout: google.translate.TranslateElement.InlineLayout.SIMPLE}, 'google_translate_element');
     }
 </script><script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
 <h3>Contact informatie</h3>
+<div class="notranslate">
 <?php
 include_once '../Php/DatabaseInformation.php';
 print("<div ");
@@ -12,3 +14,4 @@ if (isset($_SESSION['logged_in'])) {
 }
 print("<id = 'textID2'>" . loadTextFromDB(2) . "</div>");
 ?>
+</div>
