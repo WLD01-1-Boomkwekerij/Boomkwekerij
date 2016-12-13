2fc1e80304dd9998d8fd32ae60368503e439a8f7
Vescatur <ivan@edmi.nl>
2016-12-13 12:13:00 +0100

    regel updaten

---
+++ Php/UpdateRegel.php
@@ -0,0 +1,14 @@
+<?php
+if(isset($_POST['category'])){
+    $naam = $_POST['naam'];                              
+
+    doSQL("INSERT INTO category (`CategoryNaam`) 
+        VALUES ('$naam')");
+}
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
