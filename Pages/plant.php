d729149b8f5f6bc3b9d65980e177faa71eb698ce
Vescatur <ivan@edmi.nl>
2016-12-16 13:03:48 +0100

    stuf

--- Pages/plant.php
+++ Pages/plant.php
@@ -10,7 +10,7 @@
         <link href="../Css/PlantStyle.css" rel="stylesheet" type="text/css">
         <script type="text/javascript" src="//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"></script>
         <script src='https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js'></script>
-        <?php
+        <?php  
         session_start();
 
         include_once '../Php/DatabaseInformation.php';
