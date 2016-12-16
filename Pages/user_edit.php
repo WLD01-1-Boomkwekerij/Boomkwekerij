a5c30399fad1383a2c1f4c93f4c9be2c5cd72792
Erwin Jacobs <erwinpj@outlook.com>
2016-12-16 14:14:17 +0100

    check da stuffs

--- Pages/user_edit.php
+++ Pages/user_edit.php
@@ -29,11 +29,93 @@
                     $gebruiker = $_POST["gebruiker"];
                     if (isset($_POST["submit"])) {
                         include '../Php/POST.php';
-                        if ($_POST['submit'] == 'Bewerken') {
-                            if ($_POST['Wachtwoord1'] != $_POST['Wachtwoord2']) {
-                                print ('De wachtwoorden zijn niet evenlang');
-                            } elseif (count($errors) > 0) {
+                        if ($submit == 'Bewerken') {
+                            if (count($errors) > 0) {
                                 print_r(implode($errors, '<br>'));
+                                ?>
+                                }
+                                <form method="post" action="../pages/user_edit.php">
+                                    <?php
+                                    include_once '../Php/Database.php';
+                                    $array = getSQLArray('SELECT * FROM boomkwekerij.gebruiker WHERE GebruikerID =' . $gebruiker);
+                                    while ($data = $array->fetch()) {
+                                        print('<h5>' . $data['Naam'] . '</h5>');
+                                        ?>
+                                        <table>
+                                            <tr>
+                                                <th>Naam</th>
+                                                <th>Email</th>
+                                                <th>Krijgt notifactie</th>
+                                                <th>Wachtwoord</th>
+                                                <th>Wachtwoord opnieuw</th>
+                                                <th>Rechten</th>
+                                                <th>Bewerken</th>
+                                                <th>Verwijderen</th>
+                                            </tr>
+                                            <tr>
+                                                <td><input name="gebr_naam" id="gebr_naam" type="text" tabindex="1" required value="<?php print($data['Naam']); ?>"></td>
+                                                <td><input name="gebr_mail" id="gebr_mail" type="email" tabindex="2" required value="<?php print($data['Email']); ?>"></td>
+                                                <td>
+                                                    <?php
+                                                    if ($data['KrijgtEmail'] == 0) {
+                                                        ?>
+                                                        <select name="krijgt_mail" tabindex="3">
+                                                            <option value="0" selected="selected">Nee</option>
+                                                            <option value="1">Ja</option>
+                                                        </select> 
+                                                        <?php
+                                                    } else {
+                                                        ?>
+                                                        <select name="krijgt_mail" tabindex="3">
+                                                            <option value="0">Nee</option>
+                                                            <option value="1" selected="selected">Ja</option>
+                                                        </select> 
+                                                    <?php } ?>
+                                                </td>
+                                                <td><input name="Wachtwoord1" id="Wachtwoord1" type="password" tabindex="4" ></td>
+                                                <td><input name="Wachtwoord2" id="Wachtwoord2" type="password" tabindex="5" ></td>
+                                                <td> 
+                                                    <?php
+                                                    if ($data['Rol'] == '1') {
+                                                        ?>          
+                                                        <select name="rol" tabindex="6">
+                                                            <option value="beheerder" selected="selected">Beheerder</option>
+                                                            <option value="medewerker">Medewerker</option>
+                                                            <option value="vertaler">Vertaler</option>
+                                                        </select> 
+                                                        <?php
+                                                    } elseif ($data['Rol'] == '2') {
+                                                        ?>          
+                                                        <select name="rol" tabindex="6">
+                                                            <option value="beheerder" >Beheerder</option>
+                                                            <option value="medewerker" selected="selected">Medewerker</option>
+                                                            <option value="vertaler">Vertaler</option>
+                                                        </select> 
+                                                        <?php
+                                                    } elseif ($data['Rol'] == '3') {
+                                                        ?>             
+                                                        <select name="rol" tabindex="6">
+                                                            <option value="beheerder" >Beheerder</option>
+                                                            <option value="medewerker">Medewerker</option>
+                                                            <option value="vertaler" selected="selected">Vertaler</option>
+                                                        </select> 
+                                                        <?php
+                                                    }
+                                                    ?>                                    
+                                                </td>
+                                                <td>
+                                                    <input type='hidden' name='gebruiker' value="<?php print($gebruiker); ?>" />
+                                                    <input type="submit" name="submit" value="Bewerken"/>
+                                                </td>
+                                                <td>
+                                                    <input type='hidden' name='gebruiker' value="<?php print($gebruiker); ?>" />
+                                                    <input type="submit" name="submit" value="Verwijderen"/>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </form>
+                                    <?php
+                                }
                             } else {
                                 print('Wilt u de volgende gebruiker bewerken?<br>'
                                         . 'Naam: ' . $gebr_naam . '<br>'
@@ -48,14 +130,14 @@
                                 ?>
                                 <form action="../Php/user_update_delete.php" method="post">
                                     <input type='hidden' name='input_name' value="<?php echo htmlentities(serialize($_POST)); ?>" />
-                                    <input type="submit" name="submit" value="Toevoegen"/>
+                                    <input type="submit" name="submit" value="bewerken"/>
                                 </form>
                                 <form action="../Pages/logged_in.php" method="post">
                                     <input type="submit" name="cancel" value="Annuleren"/>
                                 </form>
                                 <?php
                             }
-                        } elseif ($_POST['submit'] == 'Verwijderen') {
+                        } elseif ($submit == 'Verwijderen') {
                             print('Weet u zeker dat u ' . $gebr_naam . ' wilt verwijderen? <br>');
                             ?>
                             <form action="../Php/user_update_delete.php" method="post">
@@ -66,91 +148,9 @@
                                 <input type="submit" name="cancel" value="Annuleren"/>
                             </form>
                             <?php
-                        
                         }
                     }
                     ?>
-                    <form method="post" action="../pages/user_edit.php">
-                        <?php
-                        include_once '../Php/Database.php';
-                        $array = getSQLArray('SELECT * FROM boomkwekerij.gebruiker WHERE GebruikerID =' . $gebruiker);
-                        while ($data = $array->fetch()) {
-                            print('<h5>' . $data['Naam'] . '</h5>');
-                            ?>
-                            <table>
-                                <tr>
-                                    <th>Naam</th>
-                                    <th>Email</th>
-                                    <th>Krijgt notifactie</th>
-                                    <th>Wachtwoord</th>
-                                    <th>Wachtwoord opnieuw</th>
-                                    <th>Rechten</th>
-                                    <th>Bewerken</th>
-                                    <th>Verwijderen</th>
-                                </tr>
-                                <tr>
-                                    <td><input name="gebr_naam" id="gebr_naam" type="text" tabindex="1" required value="<?php print($data['Naam']); ?>"></td>
-                                    <td><input name="gebr_mail" id="gebr_mail" type="email" tabindex="2" required value="<?php print($data['Email']); ?>"></td>
-                                    <td>
-                                        <?php
-                                        if ($data['KrijgtEmail'] = 0) {
-                                            ?>
-                                            <select name="krijgt_mail" tabindex="3">
-                                                <option value="0" selected="selected">Nee</option>
-                                                <option value="1">Ja</option>
-                                            </select> 
-                                            <?php
-                                        } else {
-                                            ?>
-                                            <select name="krijgt_mail" tabindex="3">
-                                                <option value="0">Nee</option>
-                                                <option value="1" selected="selected">Ja</option>
-                                            </select> 
-                                        <?php } ?>
-                                    </td>
-                                    <td><input name="Wachtwoord1" id="Wachtwoord1" type="password" tabindex="4" ></td>
-                                    <td><input name="Wachtwoord2" id="Wachtwoord2" type="password" tabindex="5" ></td>
-                                    <td> 
-                                        <?php
-                                        if ($data['Rol'] == '1') {
-                                            ?>          
-                                            <select name="rol" tabindex="6">
-                                                <option value="beheerder" selected="selected">Beheerder</option>
-                                                <option value="medewerker">Medewerker</option>
-                                                <option value="vertaler">Vertaler</option>
-                                            </select> 
-                                            <?php
-                                        } elseif ($data['Rol'] == '2') {
-                                            ?>          
-                                            <select name="rol" tabindex="6">
-                                                <option value="beheerder" >Beheerder</option>
-                                                <option value="medewerker" selected="selected">Medewerker</option>
-                                                <option value="vertaler">Vertaler</option>
-                                            </select> 
-                                            <?php
-                                        } elseif ($data['Rol'] == '3') {
-                                            ?>             
-                                            <select name="rol" tabindex="6">
-                                                <option value="beheerder" >Beheerder</option>
-                                                <option value="medewerker">Medewerker</option>
-                                                <option value="vertaler" selected="selected">Vertaler</option>
-                                            </select> 
-                                            <?php
-                                        }
-                                        ?>                                    
-                                    </td>
-                                    <td>
-                                        <input type='hidden' name='gebruiker' value="<?php print($gebruiker); ?>" />
-                                        <input type="submit" name="submit" value="Bewerken"/>
-                                    </td>
-                                    <td>
-                                        <input type='hidden' name='gebruiker' value="<?php print($gebruiker); ?>" />
-                                        <input type="submit" name="submit" value="Verwijderen"/>
-                                    </td>
-                                </tr>
-                            </table>
-                        <?php } ?>
-                    </form>
                 </section>
             </section>
         </section>
--- Php/POST.php
+++ Php/POST.php
@@ -4,27 +4,31 @@ if (isset($_POST['submit'])) {
     $submit = $_POST['submit'];
     $errors = array();
     if (isset($_POST['Wachtwoord1'])) {
-        if (!empty($_POST['Wachtwoord1'])) {
-            $Wachtwoord1 = $_POST['Wachtwoord1'];
-            if (!preg_match('/^[A-Za-z0-9#@%&-_]*$/', $Wachtwoord1)) {
-                $errors[1] = 'Ongeldige tekens';
+        if ($_POST['Wachtwoord1'] != $_POST['Wachtwoord2']) {
+            $errors[1] = 'Wachtwoorden zijn niet evenlang';
+        } else {
+            if (!empty($_POST['Wachtwoord1'])) {
+                $Wachtwoord1 = $_POST['Wachtwoord1'];
+                if (!preg_match('/^[A-Za-z0-9#@%&-_]*$/', $Wachtwoord1)) {
+                    $errors[1] = 'Ongeldige tekens';
+                }
+                if (strlen($Wachtwoord1) < 8) {
+                    $errors[2] = 'Wachtwoord is te kort';
+                }
+                if (preg_match('/\d/', $Wachtwoord1) == 0) {
+                    $errors[3] = 'Gebruik minimaal 1 getal';
+                }
+                if (preg_match('/[# @ % & \ - _ ]/', $Wachtwoord1) == 0) {
+                    $errors[4] = 'Gerbuik minimaal 1 van de volgende tekens: <i># @ % & - _ </i>';
+                }
+                if (preg_match('/[A-Z]/', $Wachtwoord1) == 0) {
+                    $errors[5] = 'Gebruik minimaal 1 hoofdletter';
+                }
+                if (preg_match('/[a-z]/', $Wachtwoord1) == 0) {
+                    $errors[6] = 'Gebruik minimaal 1 kleine letter';
+                }
+                $Wachtwoord1 = hash('sha256', $Wachtwoord1);
             }
-            if (strlen($Wachtwoord1) < 8) {
-                $errors[2] = 'Wachtwoord is te kort';
-            }
-            if (preg_match('/\d/', $Wachtwoord1) == 0) {
-                $errors[3] = 'Gebruik minimaal 1 getal';
-            }
-            if (preg_match('/[# @ % & \ - _ ]/', $Wachtwoord1) == 0) {
-                $errors[4] = 'Gerbuik minimaal 1 van de volgende tekens: <i># @ % & - _ </i>';
-            }
-            if (preg_match('/[A-Z]/', $Wachtwoord1) == 0) {
-                $errors[5] = 'Gebruik minimaal 1 hoofdletter';
-            }
-            if (preg_match('/[a-z]/', $Wachtwoord1) == 0) {
-                $errors[6] = 'Gebruik minimaal 1 kleine letter';
-            }
-            $Wachtwoord1 = hash('sha256', $Wachtwoord1);
         }
     }
     if (isset($_POST['Wachtwoord2'])) {
@@ -39,12 +43,11 @@ if (isset($_POST['submit'])) {
     if (isset($_POST['gebr_mail'])) {
         $gebr_mail = $_POST['gebr_mail'];
     }
-     if (isset($_POST['krijgt_mail'])) {
+    if (isset($_POST['krijgt_mail'])) {
         $krijgt_mail = $_POST['krijgt_mail'];
     }
-     if (isset($_POST['rol'])) {
+    if (isset($_POST['rol'])) {
         $rol = $_POST['rol'];
     }
-    
 }
 ?>
