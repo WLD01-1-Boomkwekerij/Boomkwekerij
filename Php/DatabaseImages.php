09ca3137d93e18f6bd4cfa50f00ae49e33c58c8d
foepje12 <jellevanommen@hotmail.nl>
2017-01-03 13:30:53 +0100

    verwijderen werkt

--- Php/DatabaseImages.php
+++ Php/DatabaseImages.php
@@ -4,7 +4,7 @@ include_once 'Database.php';
 
 //The file for all Image Commands
 //Insert Image
-function insertImage($name, $url){
+function insertImage($name, $url) {
     ProtectedDoSQL("INSERT INTO foto (FotoUrl, FotoNaam) VALUES (?, ?)", array($url, $name));
 }
 
@@ -19,7 +19,31 @@ if (isset($_GET["updateImageByName"], $_GET["oldImageUrl"], $_GET["newImageUrl"]
     ProtectedDoSQL("UPDATE foto SET FotoNaam = ? , FotoUrl = ? WHERE FotoNaam = ?", array($_GET["updateImageByName"], $_GET["newImageUrl"], $_GET["updateImageByName"]));
 }
 
+function rrmdir($dir) {
+    if (is_dir($dir)) {
+        $objects = scandir($dir);
+        foreach ($objects as $object) {
+            if ($object != "." && $object != "..") {
+                if (filetype($dir . "/" . $object) == "dir")
+                    rrmdir($dir . "/" . $object);
+                else
+                    unlink($dir . "/" . $object);
+            }
+        }
+        reset($objects);
+        rmdir($dir);
+    }
+}
+
 //Delete
-if (isset($_GET["deleteImageByName"])) {
-    ProtectedDoSQL("DELETE FROM foto WHERE FotoNaam = ?", array($_GET["deleteImageByName"]));
+if (isset($_GET["directory"], $_GET["deleteImageByName"], $_GET["type"])) {
+
+    if ($_GET["type"] == "file") {
+        unlink($_GET["directory"] . "/" . $_GET["deleteImageByName"]);
+        ProtectedDoSQL("DELETE FROM foto WHERE FotoNaam = ?", array($_GET["deleteImageByName"]));
+    }
+    else {
+        rrmdir($_GET["directory"]);
+        ProtectedDoSQL("DELETE FROM `foto` WHERE FotoUrl LIKE ?", array($_GET["directory"]));
+    }
 }
