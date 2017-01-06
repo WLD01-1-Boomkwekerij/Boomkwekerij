e0f5696ac96f699c21d867e2a53cb5dc42ebdf41
michael <michael99access@outlook.com>
2017-01-06 16:17:40 +0100

    Duidelijkheid

--- Php/footer.php
+++ Php/footer.php
@@ -1,8 +1,11 @@
 <section  class="notranslate" id="footer">
     <?php
+    // Controleert of de gebruiker is ingelogd.
+    // ls de gebruiker al is ingelogd dan word er "Uitloggen" weergeven
     if (isset($_SESSION['logged_in'])) {
         print("<li><a href='../Php/loggout.php'>Uitloggen</a></li>");
     } else {
+        // Als de gebruiker nog niet is ingelogd, dan word er "Inloggen" weergeven.
         print("<li><a href='login.php'>Inloggen</a></li>");
     }
     ?>
