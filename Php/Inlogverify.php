9dce9a8356e41587b0d486bad4f672bb5e9af08f
michael <michael99access@outlook.com>
2017-01-06 16:00:57 +0100

    Foutjes van anderen fixen

--- Php/Inlogverify.php
+++ Php/Inlogverify.php
@@ -1,18 +1,22 @@
 <?php
 
+
 function confirmIP($ip)
 {
     include_once '../Php/Database.php';
+    // Tijdzone instellen
     date_default_timezone_set('Europe/Amsterdam');
     $tijd = time();
     $query = 'SELECT `Attempts` FROM `LoginAttempts` WHERE IP = ?';
     $variable = array($ip);
     $data = ProtectedGetSQL($query, 'Attempts', $variable);
 
+    //Kijkt wanneer de gebruiker voor het laatst is ingelogd
     $query = 'SELECT `LastLogin` FROM `LoginAttempts` WHERE IP = ?';
     $timestamp = ProtectedGetSQL($query, 'LastLogin', $variable);
 
     $tijdverschil = ($tijd - $timestamp);
+    // Voert de inlogpogingen in, in de database
     if ($data == 0)
     {
         $query = 'INSERT INTO `LoginAttempts`(`IP`, `Attempts`, `LastLogin`) VALUES (?, 1, ?)';
@@ -20,6 +24,7 @@ function confirmIP($ip)
         ProtectedDoSQL($query, $variable);
         $data = 1;
     }
+    // Kijkt of er 300 seconden (5 minuten) zijn verstreken
     elseif ($tijdverschil >= 300)
     {
         $data = 1;
@@ -27,6 +32,7 @@ function confirmIP($ip)
         $variable = array($data, $tijd, $ip);
         ProtectedDoSQL($query, $variable);
     }
+    // Als de 5 minuten niet verstreken zijn -> voer een nieuw poging to in de database
     else
     {
         $data = $data + 1;
