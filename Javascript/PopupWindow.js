d8e1dcf2991da0b44822d63e5fedb4a09c614361
foepje12 <jellevanommen@hotmail.nl>
2017-01-22 23:43:55 +0100

    aanpassing

--- Javascript/PopupWindow.js
+++ Javascript/PopupWindow.js
@@ -5,51 +5,50 @@ function createBaseWindow(messageType)
     backgroundDiv.id = "popupBackgroundDiv";
     $(backgroundDiv).addClass("PopupWindow");
     document.body.appendChild(backgroundDiv);
-    
+
     var titleDiv = createElement("div");
     titleDiv.id = "popupTitleDiv";
     $(titleDiv).addClass("PopupWindow");
     backgroundDiv.appendChild(titleDiv);
-    
+
     var titleText = createElement("p");
     titleText.id = "popupTitleText";
     $(titleText).addClass("PopupWindow");
     titleDiv.appendChild(titleText);
-    
-    switch(messageType)
+
+    switch (messageType)
     {
-        case "Error": 
+        case "Error":
             backgroundDiv.style.backgroundColor = "#E94B35";
             titleDiv.style.backgroundColor = "#D64531";
             titleText.style.color = "#E1EDD8";
             titleText.innerHTML = "Error";
     }
-    
+
     var innerDiv = createElement("div");
     innerDiv.id = "popupInnerDiv";
     $(innerDiv).addClass("PopupWindow");
     backgroundDiv.appendChild(innerDiv);
-    
+
     return innerDiv;
 }
 
 function createPopupError(message)
 {
     var innerDiv = createBaseWindow("Error");
-    
+
     var errorMessage = createElement("p");
     errorMessage.id = "popupMessage";
     $(errorMessage).addClass("PopupWindow");
     errorMessage.innerHTML = message;
     innerDiv.appendChild(errorMessage);
-    
+
 }
 
 $(document).ready(function ()
 {
     $(".PopupWindow").click(function ()
     {
-        console.log("test");
         $(".PopupWindow").animate({top: "-320px"});
 
     });
